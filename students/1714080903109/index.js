4e4ba452a5345ed774cbb7495b34546a20436df5
1076982049 <1076982049@qq.com>
2019-04-14 00:21:52 +0800

    index.js

---
+++ students/1714080903109/index.js
@@ -0,0 +1,9 @@
+Page({
+  data: {
+    "imgUrls": [
+      "http://wx2.sinaimg.cn/large/0060lm7Tly1g2139sfvcyj30u00da3yv.jpg",
+      "http://wx1.sinaimg.cn/large/0060lm7Tly1g213ad5sg8j30ty0dajrn.jpg",
+      "http://wx1.sinaimg.cn/large/0060lm7Tly1g213albo88j30u00dc74u.jpg"
+    ]
+  }
+})

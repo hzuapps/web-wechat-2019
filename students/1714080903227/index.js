f93d576dafb7d62b9e2bbf782ab9dd3b91819c9e
zhujingyi2019 <1264156129@qq.com>
2019-04-20 15:05:11 +0800

    shiyan6

--- students/1714080903227/index.js
+++ students/1714080903227/index.js
@@ -9,47 +9,28 @@ Page({
     canIUse: wx.canIUse('button.open-type.getUserInfo'),
 
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
-  },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
+ onLoad: function () {
+    var _this = this;
+    wx.getSystemInfo({
+      success: function (res) {
+        _this.setData({
+          screenHeight: res.windowHeight,
+          screenWidth: res.windowWidth,
+        });
       }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+    });
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
+  imageLoad: function (e) {
+    var _this = this;
+    var $width = e.detail.width,  //获取图片真实宽度
+      $height = e.detail.height,
+      ratio = $width / $height;  //图片的真实宽高比例
+    var viewWidth = 800 ,      //设置图片显示宽度，
+      viewHeight = 500 / ratio;  //计算的高度值
     this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+      imgwidth: viewWidth,
+      imgheight: viewHeight
     })
-  },
+  }
  
 })

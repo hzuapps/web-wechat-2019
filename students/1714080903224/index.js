c02be4a080362e53c1a34c24a9b0df0734a1aeff
zhouzzfs <1227559593@qq.com>
2019-05-07 22:41:41 +0800

    实验七

--- students/1714080903224/index.js
+++ students/1714080903224/index.js
@@ -3,61 +3,51 @@
 const app = getApp()
 
 Page({
+  /**
+   * 页面的初始数
+   */
   data: {
-    plain: true,
-  },//按钮边框
+    plain: true,//按钮边框
+   
+  },
 
-  //事件处理函数
-  bindViewTap: function () {
-    wx.navigateTo({
-      url: '../logs/logs'
+  /**
+   * 首页页面跳转
+   */
+  jumpPage1: function () {
+    wx.redirectTo({
+      url: '../chaju/chaju',
     })
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse) {
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
-    }
+  jumpPage2: function () {
+    wx.navigateTo({
+      url: '../chami/chami',
+    })
   },
+  jumpPage3: function () {
+    wx.navigateTo({
+      url: '../chongcha/chongcha',
+    })
+  },
+  jumpPage4: function () {
+    wx.navigateTo({
+      url: '../lishi/lishi',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad:function(options){},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady:function(){},
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow:function(){},
+
   
-  getUserInfo: function (e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
-    })
-  },
-
-  jumpPage:function()
-  {
-    wx.navigateTo({
-      url: 'pages/search/search',
-    })
-  },
-
-
 
 })

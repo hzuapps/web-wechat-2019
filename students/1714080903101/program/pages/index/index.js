42c3c83114a0fca384fa51c449ae0ce291a5bfb7
jasper19981103 <1336404397@qq.com>
2019-05-10 16:36:10 +0800

    创建首页

---
+++ students/1714080903101/program/pages/index/index.js
@@ -0,0 +1,85 @@
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    "首页": "Welcome to Home Page!",
+    "grids": [0, 1, 2],
+  grids: [{
+        name: "Calendar",
+        url: "../../pages/logs/logs",
+        img: "../../images/75.png"
+      },
+      {
+        name: "Plan",
+        url: "../../pages/plan/plan",
+        img: "../../images/14.png"
+      },
+    {
+      name: "Record",
+      url: "../../page/media/index",
+      img: "../../images/76.png"
+    },
+    ]
+  },
+
+  /**
+     * 生命周期函数--监听页面加载
+     */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
+
+  

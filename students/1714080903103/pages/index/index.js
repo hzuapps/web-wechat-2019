51b4a8e9ff2f535a9eeb6f23556bdebd67305594
aoaoaoe <598213929@qq.com>
2019-05-10 23:33:34 +0800

    #7 #7

--- students/1714080903103/pages/index/index.js
+++ students/1714080903103/pages/index/index.js
@@ -1,83 +1,83 @@
+//index.js
+//获取应用实例
+const app = getApp()
 
 Page({
   data: {
-    list: [
-      {
-        id: 'Monday',
-        name: '周一',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Tuesday',
-        name: '周二',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Wednesday',
-        name: '周三',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Thursday',
-        name: '周四',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Friday',
-        name: '周五',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Sarturday',
-        name: '周六',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      },
-      {
-        id: 'Sunday',
-        name: '周日',
-        open: false,
-        pages: ['AddMemorandum', 'AddMemorandum']
-      }
-    ]
+    inputValue: '',
+    addTel: '',
   },
-  showInput: function () {
+  bindKeyInput: function (e) {
     this.setData({
-      inputShowed: true
-    });
+      inputValue: e.detail.value
+    })
   },
-  hideInput: function () {
-    this.setData({
-      inputVal: "",
-      inputShowed: false
-    });
-  },
-  clearInput: function () {
-    this.setData({
-      inputVal: ""
-    });
-  },
-  inputTyping: function (e) {
-    this.setData({
-      inputVal: e.detail.value
-    });
-  },
-  kindToggle: function (e) {
-    var id = e.currentTarget.id, list = this.data.list;
-    for (var i = 0, len = list.length; i < len; ++i) {
-      if (list[i].id == id) {
-        list[i].open = !list[i].open
-      } else {
-        list[i].open = false
-      }
+  ontouch1: function () {
+    wx.redirectTo({
+      url: '../index/index',
+    })
+
+    if (this.data.inputValue) {
+      wx.setStorage({
+        key: "addTel",
+        data: this.data.inputValue,
+      })
+    } else {
+      wx.showModal({
+        title: '手机号为空',
+        content: '请输入手机号码',
+        success: function (res) {
+          if (res.confirm) {
+            console.log('用户点击确认')
+          }
+        }
+      })
     }
-    this.setData({
-      list: list
-    });
-  }
-});
+    
+  },
+
+  onShow: function () {
+    var that = this;
+   
+    wx.getStorage({
+      key: 'addTel',
+      success: function (res) {
+        console.log(res.data)
+        that.setData({
+          addTel: res.data
+          
+        })     
+      }
+    })
+   
+  },
+  
+  ontouch2: function (e) {
+    wx.downloadFile({
+      url: "http://a.hiphotos.baidu.com/image/pic/item/838ba61ea8d3fd1fc9c7b6853a4e251f94ca5f46.jpg",
+      success: function (res) {
+        console.log(res);
+        var rr = res.tempFilePath;
+        wx.saveImageToPhotosAlbum({
+
+          filePath: rr,
+          success(res) {
+            wx.showToast({
+              title: '保存成功',
+              icon: 'success',
+              duration: 2000
+            })
+          }
+        })
+      }
+    })
+  },
+
+  ontouch3:function(e){
+    wx.showToast({
+      title: '成功',
+      icon: 'success',
+      duration: 2000
+    })
+  },
+})

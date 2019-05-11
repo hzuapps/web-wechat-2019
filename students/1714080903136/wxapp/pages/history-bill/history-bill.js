d5483a7f80567c015c0589c2e77a322d765976aa
Cageclt <929749918@qq.com>
2019-05-11 21:47:08 +0800

    #7 #444

---
+++ students/1714080903136/wxapp/pages/history-bill/history-bill.js
@@ -0,0 +1,25 @@
+// pages/history-bill/history-bill.js
+Page({
+  data:{
+    startdDate:"2017-04-23",
+    endDate:"2017-04-23",
+    today:"2017-04-23",
+    scrollHeight:"100%",
+  },
+  onLoad:function(options){
+    // 页面初始化 options为页面跳转所带来的参数
+
+  },
+  onReady:function(){
+    // 页面渲染完成
+  },
+  onShow:function(){
+    // 页面显示
+  },
+  onHide:function(){
+    // 页面隐藏
+  },
+  onUnload:function(){
+    // 页面关闭
+  }
+})

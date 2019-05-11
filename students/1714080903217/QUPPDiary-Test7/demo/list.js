6c3c80ebcbe6e0b7ec1cc91b9f6e6065c2d3130a
quppenge <287241724@qq.com>
2019-05-11 14:57:37 +0800

    Test7

---
+++ students/1714080903217/QUPPDiary-Test7/demo/list.js
@@ -0,0 +1,56 @@
+var data = {
+    "diaries": [
+        {
+        "image": "http://zhuoshourian.top/111.jpg?imageView2/1/w/620/h/330/format/jpg",
+            "avatar": "http://zhuoshourian.top/1.png",
+            "title": "日记模板",
+            "meta": "2013年8月10日 / 6天 / 476图",
+            "nickname": "SmallClock",
+        },
+        {
+          "image": "http://zhuoshourian.top/111.jpg",
+          "avatar": "http://zhuoshourian.top/1.png",
+            "title": "、艺术与兔子",
+            "meta": "2019年9月1日 / 6天 / 476图",
+            "nickname": "TinyClock",
+        },
+        {
+          "image": "http://zhuoshourian.top/123.jpg",
+          "avatar": "http://zhuoshourian.top/1.png",
+            "title": "濑户内海北半圈-历史",
+            "meta": "2019年9月1日 / 6天 / 476图",
+            "nickname": "用户1",
+        },
+        {
+          "image": "http://zhuoshourian.top/111.jpg",
+          "avatar": "http://zhuoshourian.top/1.png",
+            "title": "文章1",
+            "meta": "2019年9月1日 / 6天 / 476图",
+            "nickname": "Bear",
+        },
+        {
+          "image": "http://zhuoshourian.top/123.jpg",
+          "avatar": "http://zhuoshourian.top/1.png",
+            "title": "网上随便找的日记1",
+            "meta": "2019年9月1日 / 6天 / 476图",
+          "nickname":"日本名字",
+        },
+        {
+          "image": "http://zhuoshourian.top/111.jpg",
+          "avatar": "http://zhuoshourian.top/1.png",
+          "title": "网上随便找的日记2",
+            "meta": "2019年9月1日 / 6天 / 476图",
+            "nickname": "qupp用户1",
+        }
+    ],
+
+    dairys: [
+        {
+        "cover": "http://zhuoshourian.top/111.jpg",
+        }
+    ]
+}
+
+module.exports = {
+    "data": data
+}

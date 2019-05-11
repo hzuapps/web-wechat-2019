1e3d3b5a8163a6fc7de30d6cc79a3d90e0320871
zhouzzfs <1227559593@qq.com>
2019-05-11 17:25:26 +0800

    食茶

---
+++ students/1714080903224/食茶/pages/index/index.js
@@ -0,0 +1,53 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  /**
+   * 页面的初始数
+   */
+  data: {
+    plain: true,//按钮边框
+   
+  },
+
+  /**
+   * 首页页面跳转
+   */
+  jumpPage1: function () {
+    wx.navigateTo({
+      url: '../chaju/chaju',
+    })
+  },
+  jumpPage2: function () {
+    wx.navigateTo({
+      url: '../chami/chami',
+    })
+  },
+  jumpPage3: function () {
+    wx.navigateTo({
+      url: '../chongcha/chongcha',
+    })
+  },
+  jumpPage4: function () {
+    wx.navigateTo({
+      url: '../lishi/lishi',
+    })
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad:function(options){},
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady:function(){},
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow:function(){},
+
+  
+
+})
